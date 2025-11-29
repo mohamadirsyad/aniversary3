c4049ef249e42791ccef73c179cf22c2aa274130
Mohamamad-Irsyad <124248564+Mohamamad-Irsyad@users.noreply.github.com>
2025-11-30 04:36:46 +0700

    3

--- src/three/FlowerScene.jsx
+++ src/three/FlowerScene.jsx
@@ -5,24 +5,29 @@ import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 export default function FlowerScene() {
   return (
-    <div className="w-full h-[350px] md:h-[450px]">
-      <p>Ada Bunga buat kamu hehehe💗</p>
-      <Canvas camera={{ position: [2, 2, 4] }}>
-        <ambientLight intensity={0.6} />
-        <pointLight position={[3, 3, 3]} intensity={1.5} />
+    <div className="flex justify-center">
+      <div className="w-[250px] md:w-full h-[350px] md:h-[450px] bg-gradient-to-b from-pink-200 to-pink-400 rounded-xl shadow-xl p-2">
+        <p className="text-white text-center font-semibold mb-2">
+          Ada Bunga buat kamu hehehe💗
+        </p>
 
-        <FlowerGrow position={[0, -0.5, 0]} />
+        <Canvas camera={{ position: [2, 2, 4] }}>
+          <ambientLight intensity={0.6} />
+          <pointLight position={[3, 3, 3]} intensity={1.5} />
 
-        <OrbitControls enableZoom={false} enablePan={false} />
+          <FlowerGrow position={[0, -0.5, 0]} />
 
-        <EffectComposer>
-          <Bloom
-            intensity={1.3}
-            luminanceThreshold={0.2}
-            luminanceSmoothing={0.9}
-          />
-        </EffectComposer>
-      </Canvas>
+          <OrbitControls enableZoom={false} enablePan={false} />
+
+          <EffectComposer>
+            <Bloom
+              intensity={1.3}
+              luminanceThreshold={0.2}
+              luminanceSmoothing={0.9}
+            />
+          </EffectComposer>
+        </Canvas>
+      </div>
     </div>
   );
 }
