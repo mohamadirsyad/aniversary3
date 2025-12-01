0192a7af221f2acd9ee97826f2b8af65677c86f1
Mohamamad-Irsyad <124248564+Mohamamad-Irsyad@users.noreply.github.com>
2025-12-01 21:37:25 +0700

    add link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router";
+
 const Footer = () => {
   return (
     <footer className="mt-20 py-8 bg-white border-t">
       <div className="max-w-4xl mx-auto px-4 text-center text-gray-500">
-        © {new Date().getFullYear()} Untukmu — Happy 3rd Anniversary ❤️
+        © {new Date().getFullYear()} Untukmu —
+        <Link to="/admin">Happy 3rd Anniversary</Link> ❤️
       </div>
     </footer>
   );
